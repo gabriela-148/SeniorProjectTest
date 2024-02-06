df69e7915b3a4919de77db64885d8c41282d6605
Gabi <gh734295@sju.edu>
2024-02-06 15:04:32 -0500

    Initial Commit

---
+++ SeniorProjectTest/SeniorProjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorProjectTestApp.swift
+//  SeniorProjectTest
+//
+//  Created by Gabriella Huegel on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SeniorProjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
